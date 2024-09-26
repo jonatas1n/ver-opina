3ce4de37d08222115b38dade64adb0a8c2d99c75
jonatas1n <jonatasgeek@gmail.com>
2024-09-26 12:26:52 -0300

    update

--- landing/landing/settings/base.py
+++ landing/landing/settings/base.py
@@ -106,8 +106,7 @@ WSGI_APPLICATION = "landing.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default="postgres://postgres:postgres@localhost:5432/ver_opina",
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
