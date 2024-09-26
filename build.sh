9ba24d882420f4b6dba07200f51de36cb65168b1
jonatas1n <jonatasgeek@gmail.com>
2024-09-26 14:40:26 -0300

    remove all poetry run prefix

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ echo "source $(poetry env info --path)/bin/activate" >> /root/.bashrc
 source $(poetry env info --path)/bin/activate
 
 python manage.py migrate
-poetry run python manage.py migrate
-poetry run python manage.py findstatic .
-poetry run python manage.py collectstatic --noinput
-poetry run poetry install
-poetry run poetry add gunicorn
-poetry run gunicorn landing.wsgi:application --bind 0.0.0.0:$PORT
+python manage.py migrate
+python manage.py findstatic .
+python manage.py collectstatic --noinput
+poetry install
+poetry add gunicorn
+gunicorn landing.wsgi:application --bind 0.0.0.0:$PORT
